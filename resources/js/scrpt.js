ea6fff1845b2627ba1f7984db3f81ffbce0302f7
PriscilaCS <39917483+SuayMack@users.noreply.github.com>
2020-10-03 23:44:08 -0300

    animação cartas

--- resources/js/scrpt.js
+++ resources/js/scrpt.js
@@ -1,13 +1,26 @@
 //constante referente a todas as cartas
 const cards = document.querySelectorAll('.card')
+let hasFlipedCard = false
+let firstCard, secondCard
 
 function flipCard() {
     //adiciona e tira a classe flip ao elemento clicado
-    this.classList.toggle('flip')
+    this.classList.addEventListener('flip')
+    //se o hasFlipedCard for false
+    if(!hasFlipedCard) {
+        //setar o hasFlipedCard para true
+        hasFlipedCard = true
+        //esta é a a primeira carta
+        firstCard = this
+        return 
+    }
+    //esta é a segunda
+    secondCard = this
+    check
 }
 
 //para cada carta do nosso forEach, 
 cards.forEach((card) => {
     //para cada card clicado acionar a função flipCard
-
+    card.addEventListener('click', flipCard)
 })
